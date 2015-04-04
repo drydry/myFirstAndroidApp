7c251ac66db9fcded6e4ee03f90aa8a67259dcc8
drydry <cedric.pommepuy@gmail.com>
2015-04-04 18:41:23 -0400

    added MYSqlite connector, table for profiles, new activities to create a profile and another to list all profiles + updated main activity

--- MyFirstAndroidApp/app/src/main/java/com/example/cedric/myfirstandroidapp/MainActivity.java
+++ MyFirstAndroidApp/app/src/main/java/com/example/cedric/myfirstandroidapp/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.cedric.myfirstandroidapp;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Toast;
 
-import com.example.cedric.myfirstandroidapp.myprofile.MyProfile;
+import com.example.cedric.myfirstandroidapp.database.model.Profile;
+import com.example.cedric.myfirstandroidapp.myprofile.CreateProfileActivity;
+import com.example.cedric.myfirstandroidapp.myprofile.ListProfilesActivity;
 
-import static android.widget.TextView.*;
+import static android.widget.TextView.BufferType;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -18,19 +24,37 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
         setContentView(R.layout.activity_main);
 
-        MyProfile profile = new MyProfile( "Cedric", 29 );
+        final Button gotoCreateProfileBtn = (Button) findViewById(R.id.goto_create_profile_btn);
+        gotoCreateProfileBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                Intent intent = new Intent(MainActivity.this, CreateProfileActivity.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
 
+        final Button gotoProfilesListBtn = (Button) findViewById(R.id.button_goto_profiles_list);
+        gotoProfilesListBtn .setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                Intent intent = new Intent(MainActivity.this, ListProfilesActivity.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
+
+        // Set a new profile
+        /*Profile profile = new Profile( "Cedric", 29 );
+
+        // Get the editText fields
         EditText nameText = (EditText) findViewById(R.id.profileName);
         EditText ageText = (EditText) findViewById(R.id.profileAge);
 
-        if( null != nameText  ) nameText.setText(profile.getName(), BufferType.EDITABLE);
-
-        if( null != ageText ) ageText.setText( String.valueOf(profile.getAge()) , BufferType.EDITABLE);
-
-
+        // Set profile values into the editText fields
+        nameText.setText(profile.getName(), BufferType.EDITABLE);
+        ageText.setText( String.valueOf(profile.getAge()) , BufferType.EDITABLE);
+        */
     }
 
 
